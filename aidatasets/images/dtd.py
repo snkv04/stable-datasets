53edc201b75732cfe1ff15f6bbb26ab8bbde3df4
Leon-Leyang <leyang_hu@brown.edu>
2024-12-20 15:18:54 -0500

    Add hf-compatible dtd

--- aidatasets/images/dtd.py
+++ aidatasets/images/dtd.py
@@ -1,95 +1,90 @@
-import os
-import pickle
 import tarfile
-import time
-from ..utils import Dataset
 from io import BytesIO
 from PIL import Image
-import numpy as np
 from tqdm import tqdm
+import datasets
 
 
+class DTD(datasets.GeneratorBasedBuilder):
+    """Describable Textures Dataset (DTD)
 
-class DTD(Dataset):
-    """Image classification.
+    DTD is a texture database, consisting of 5640 images, organized according to a list of 47 terms (categories)
+    inspired from human perception. There are 120 images for each category. Image sizes range between 300x300 and
+    640x640, and the images contain at least 90% of the surface representing the category attribute. The images were
+    collected from Google and Flickr by entering our proposed attributes and related terms as search queries. The images
+    were annotated using Amazon Mechanical Turk in several iterations. For each image we provide key attribute (main
+    category) and a list of joint attributes.
 
+    The data is split in three equal parts, in train, validation and test, 40 images per class, for each split. We
+    provide the ground truth annotation for both key and joint attributes, as well as the 10 splits of the data we used
+    for evaluation.
     """
 
+    VERSION = datasets.Version("1.0.0")
 
-    @property
-    def urls(self):
-        return {
-        "dtd-r1.0.1.tar.gz": "https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz",
-        "dtd-r1.0.1-labels.tar.gz": "https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1-labels.tar.gz",
-        }
+    def _info(self):
+        return datasets.DatasetInfo(
+            description="""Describing Textures in the Wild (DTD) is a dataset for texture classification.
+                           It contains 5640 images organized into 47 categories.""",
+            features=datasets.Features(
+                {
+                    "image": datasets.Image(),
+                    "label": datasets.ClassLabel(names=[
+                        "banded", "blotchy", "braided", "bubbly", "bumpy", "chequered", "cobwebbed",
+                        "cracked", "crosshatched", "crystalline", "dotted", "fibrous", "flecked",
+                        "freckled", "frilly", "gauzy", "grid", "grooved", "honeycombed", "interlaced",
+                        "knitted", "lacelike", "lined", "marbled", "matted", "meshed", "paisley",
+                        "perforated", "pitted", "pleated", "polka-dotted", "porous", "potholed", "scaly",
+                        "smeared", "spiralled", "sprinkled", "stained", "stratified", "striped",
+                        "studded", "swirly", "veined", "waffled", "woven", "wrinkled", "zigzagged"
+                    ])
+                }
+            ),
+            supervised_keys=("image", "label"),
+            homepage="https://www.robots.ox.ac.uk/~vgg/data/dtd/",
+            citation="""@InProceedings{cimpoi14describing,
+                            Author    = {M. Cimpoi and S. Maji and I. Kokkinos and S. Mohamed and and A. Vedaldi},
+                            Title     = {Describing Textures in the Wild},
+                            Booktitle = {Proceedings of the {IEEE} Conf. on Computer Vision and Pattern Recognition ({CVPR})},
+                            Year      = {2014}}""",
+        )
 
-    @property
-    def cite(self):
-        return """
-	    @InProceedings{cimpoi14describing,
-	      Author    = {M. Cimpoi and S. Maji and I. Kokkinos and S. Mohamed and and A. Vedaldi},
-	      Title     = {Describing Textures in the Wild},
-	      Booktitle = {Proceedings of the {IEEE} Conf. on Computer Vision and Pattern Recognition ({CVPR})},
-	      Year      = {2014}}"""
+    def _split_generators(self, dl_manager):
+        archive_path = dl_manager.download(
+            "https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz"
+        )
 
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"archive_path": archive_path, "split": "train"},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={"archive_path": archive_path, "split": "val"},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={"archive_path": archive_path, "split": "test"},
+            ),
+        ]
 
-    @property
-    def num_classes(self):
-        return 47
- 
-    @property
-    def webpage(self):
-        return "https://www.robots.ox.ac.uk/~vgg/data/dtd/"
+    def _generate_examples(self, archive_path, split):
+        with tarfile.open(archive_path, "r:gz") as tar:
+            split_file = f"dtd/labels/{split}1.txt"
+            file_names = self._read_split_file(tar, split_file)
 
-    def load(self, split=1):
-        t0 = time.time()
-    
-        tar = tarfile.open(self.path / self.name / "dtd-r1.0.1-labels.tar.gz", "r:gz")
-        train_names = tar.extractfile("labels/train{split}.txt").read().decode("utf-8")
-        test_names = tar.extractfile("labels/train{split}.txt").read().decode("utf-8")
-        val_names = tar.extractfile("labels/train{split}.txt").read().decode("utf-8")
+            for idx, file_name in enumerate(tqdm(file_names, desc=f"Processing {split} split")):
+                member = tar.getmember(f"dtd/images/{file_name}")
+                file = tar.extractfile(member)
+                image = Image.open(BytesIO(file.read())).convert("RGB")
 
+                yield idx, {
+                    "image": image,
+                    "label": file_name.split("/")[0],
+                }
 
-
-    
-        # Load train set
-        print(tar.getmembers())
-        val_names = tar.extractfile("fgvc-aircraft-2013b/data/images_val.txt").read().decode("utf-8")
-        train_names = tar.extractfile("fgvc-aircraft-2013b/data/images_train.txt").read().decode("utf-8")
-        test_names = tar.extractfile("fgvc-aircraft-2013b/data/images_test.txt").read().decode("utf-8")
-        train_names = train_names.split("\n")
-        train_images, test_images, val_images = [], [], []
-        for name in tqdm(train_names, desc="Train images"):
-            im = BytesIO(tar.extractfile(f"fgvc-aircraft-2013b/data/images/{name}.jpg").read())
-            train_images.append(Image.open(im))
-        for name in tqdm(val_names, desc="Val images"):
-            im = BytesIO(tar.extractfile(f"fgvc-aircraft-2013b/data/images/{name}.jpg").read())
-            val_images.append(Image.open(im))
-        for name in tqdm(test_names, desc="Test images"):
-            im = BytesIO(tar.extractfile(f"fgvc-aircraft-2013b/data/images/{name}.jpg").read())
-            test_images.append(Image.open(im))
-
-
-
-        asdf
-        train_images = list()
-        train_labels = list()
-        for k in tqdm(range(1, 6), desc="Loading cifar10", ascii=True):
-            f = tar.extractfile("cifar-10-batches-py/data_batch_" + str(k)).read()
-            data_dic = pickle.loads(f, encoding="latin1")
-            train_images.append(data_dic["data"].reshape((-1, 3, 32, 32)))
-            train_labels.append(data_dic["labels"])
-        train_images = np.concatenate(train_images, 0)
-        train_labels = np.concatenate(train_labels, 0)
-    
-        # Load test set
-        f = tar.extractfile("cifar-10-batches-py/test_batch").read()
-        data_dic = pickle.loads(f, encoding="latin1")
-        test_images = data_dic["data"].reshape((-1, 3, 32, 32))
-        test_labels = np.array(data_dic["labels"])
-    
-        self["train_X"] =  np.transpose(train_images, (0, 2, 3, 1))
-        self["train_y"] =  train_labels
-        self["test_X"] =  np.transpose(test_images, (0, 2, 3, 1))
-        self["test_y"] = test_labels
-        print("Dataset cifar10 loaded in{0:.2f}s.".format(time.time() - t0))
+    def _read_split_file(self, tar, split_file):
+        """Helper function to read split file from the tar archive."""
+        split_content = tar.extractfile(split_file).read().decode("utf-8")
+        return split_content.splitlines()
